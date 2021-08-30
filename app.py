32ac2f1d5ec7117ae4c7c11e471e4890efc9cbbe
Bruno Lorente Cantarero <blorente@novadevs.com>
2021-08-30 13:58:22 +0200

    Minor UI fixes.

--- app.py
+++ app.py
@@ -21,60 +21,16 @@ def index():
         request.root_url = url_for('index', _external=True)
     return render_template('index.html')
 
-@app.route('/foundation/collections/<collectionId>/items')
-@app.route('/foundation/collections/<collectionId>/items/')
-def foundation_get_features(collectionId="aerofacp_1m"):
-    # Get the collection ID
-    collection_id = collectionId
-    # Set the API resource url
-    URL = "https://test.cubewerx.com/cubewerx/cubeserv/demo/ogcapi/Foundation/collections/"+collection_id+"/items"
-    f = "json" # str | A MIME type indicating the representation of the resources to be presented (e.g. application/gml+xml; version=3.2 for GML 3.2). (optional)
-    limit = 10 # int | The optional limit parameter limits the number of items that are presented in the response document.  Only items are counted that are on the first level of the collection in the response document. Nested objects contained within the explicitly requested items shall not be counted. (optional) (default to 10)
-    # defining a params dict for the parameters to be sent to the API
-    PARAMS = {
-        'f':f,
-        'limit':limit
-    }
-    # sending get request and saving the response as response object
-    api_response = requests.get(url = URL, params = PARAMS)
-    # extracting data in json format
-    json_api_response = api_response.json()
-    json_fearures_list = json_api_response["features"]
-    # Parsing to string
-    features_list = json.dumps(json_fearures_list)
-    # Returning string
-    return features_list
-
-@app.route('/foundation/collections/<collectionId>/items/<itemId>')
-@app.route('/foundation/collections/<collectionId>/items/<itemId>/')
-def foundation_get_feature(collectionId="aerofacp_1m",itemId="CWFID.AEROFACP_1M.0.0.1009C50A276905DE1F20020000"):
-    # Get the collection ID
-    collection_id = collectionId
-    # get the item ID
-    item_id = itemId
-    # Set the API resource url
-    URL = "https://test.cubewerx.com/cubewerx/cubeserv/demo/ogcapi/Foundation/collections/"+collection_id+"/items/"+item_id
-    f = "json" # str | A MIME type indicating the representation of the resources to be presented (e.g. application/gml+xml; version=3.2 for GML 3.2). (optional)
-    # defining a params dict for the parameters to be sent to the API
-    PARAMS = {
-        'f':f,
-    }
-    # sending get request and saving the response as response object
-    api_response = requests.get(url = URL, params = PARAMS)
-    # extracting data in json format
-    json_api_response = api_response.json()
-    # Parsing to string
-    features_item = json.dumps(json_api_response)
-    # Returning string
-    return features_item
-
-@app.route('/collections/<collectionId>/items')
+@app.route('/collections/<collectionId>/items/')
 def get_features(collectionId):
     # create an instance of the API class
     api_instance = openapi_client.VectorFeaturesAndCatalogueRecordsApi(openapi_client.ApiClient(configuration))
     collection_id = collectionId # str | Name of a feature collection offered by the service
     f = 'json' # str | A MIME type indicating the representation of the resources to be presented (e.g. application/gml+xml; version=3.2 for GML 3.2). (optional)
-    limit = 1000000 # int | The optional limit parameter limits the number of items that are presented in the response document.  Only items are counted that are on the first level of the collection in the response document. Nested objects contained within the explicitly requested items shall not be counted. (optional) (default to 10)
+    if (request.args.get('l')):
+        limit = int(request.args.get('l')) # int | The optional limit parameter limits the number of items that are presented in the response document.  Only items are counted that are on the first level of the collection in the response document. Nested objects contained within the explicitly requested items shall not be counted. (optional) (default to 10)
+    else:
+        limit = 20 # int | The optional limit parameter limits the number of items that are presented in the response document.  Only items are counted that are on the first level of the collection in the response document. Nested objects contained within the explicitly requested items shall not be counted. (optional) (default to 10)
     # bbox = [3.4] # list[float] | Only features that have a geometry that intersects the bounding box are selected. The bounding box is provided as four or six numbers, depending on whether the coordinate reference system includes a vertical axis (elevation or depth): * Lower left corner, coordinate axis 1 * Lower left corner, coordinate axis 2 * Lower left corner, coordinate axis 3 (optional) * Upper right corner, coordinate axis 1 * Upper right corner, coordinate axis 2 * Upper right corner, coordinate axis 3 (optional) The coordinate reference system of the values is WGS84 longitude/latitude (http://www.opengis.net/def/crs/OGC/1.3/CRS84) unless a different coordinate reference system is specified in the parameter `bbox-crs`. For WGS84 longitude/latitude the values are in most cases the sequence of minimum longitude, minimum latitude, maximum longitude and maximum latitude. However, in cases where the box spans the antimeridian the first value (west-most box edge) is larger than the third value (east-most box edge). If a feature has multiple spatial geometry properties, it is the decision of the server whether only a single spatial geometry property is used to determine the extent or all relevant geometries. (optional)
     # datetime = '2013-10-20T19:20:30+01:00' # datetime | Temporal constraint to be applied to a query; may be a time instance or a temporal period as per ISO8601. (optional)
     # t_relation = 't_relation_example' # str | Temporal operator to use for temporal contstraint (optional)
